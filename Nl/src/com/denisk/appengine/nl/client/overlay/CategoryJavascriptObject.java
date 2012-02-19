b93aa3b696f77f706b1583921b9d878237cf1ee2
denisk <denisk@denisk-home>
2012-02-19 16:14:19 +0200

    added Category JS object

---
+++ Nl/src/com/denisk/appengine/nl/client/overlay/CategoryJavascriptObject.java
@@ -0,0 +1,18 @@
+package com.denisk.appengine.nl.client.overlay;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+
+public class CategoryJavascriptObject extends JavaScriptObject {
+	protected CategoryJavascriptObject() {
+		
+	}
+
+	public final native String getName() /*-{return this.name}-*/;
+	
+	public final native String getDescription() /*-{return this.description}-*/;
+	
+	public static native JsArray<CategoryJavascriptObject> getArrayFromJson(String jsonStr)/*-{
+		return eval(jsonStr);
+	}-*/;
+}
